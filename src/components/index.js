818c9c9810f432a4e972fd24731165c6466b095b
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-15 10:40:58 -0500

    Add starter files
    Add starter files 2
    
    
    Starter Files 3

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import { Home } from "./Home/";
+import { Match } from "./Match/";
+import { PetCard } from "./PetCard/";
+import { PetSearchBar } from "./PetSearchBar/";
+import { Profile } from "./Profile/";
