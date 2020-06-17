3edcfb79b977c2c3ceca45852acc3a0c8f64c3bb
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-17 14:28:42 -0500

    Working test of font awesome

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <h1 className="fa fa-diamond">Hello World</h1>
   </React.StrictMode>,
   document.getElementById("root")
 );
