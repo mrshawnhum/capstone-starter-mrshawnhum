318b5cb5b6604e4f6b9750fe143b7df2bc17d93a
Manav Misra <manavm@visionwebsoft.com>
2020-06-19 15:54:39 -0500

    :seedling: Add starter routes for users db

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,13 +2,11 @@ import { client } from "./client";
 
 export const getAllUsers = async () => {
   try {
-    const cursor = await client.db("users").collection("users.test").find();
+    const cursor = await client.db("users").collection("users").find();
     const results = await cursor.toArray();
     await cursor.close();
     return results;
   } catch (e) {
     throw Error(e);
-  } finally {
-    await client.close();
   }
 };
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,11 +1,35 @@
+import { getAllUsers } from "db";
+
 import { Router } from "express";
 
 const router = Router();
 
+// @route GET api/users/test
+// @description tests products route
+// @access Public
 router.get("/test", (_, res) => {
   res.send("<p>Users router test</p>");
 });
 
-router.get("/users", async (_, res) => {});
+// @route POST api/users/register
+// @description Register a new user
+// @access Public
+router.post("/register", async (req, res) => {
+  res.send(`<p>Trying to register a ${req.body}!</p>`);
+});
+
+// @route POST api/users/authenticate
+// @description Authenticate a user
+// @access Public
+router.post("/authenticate", async (req, res) => {
+  res.send(`<p>Trying to authenticate ${req.body}!</p>`);
+});
+
+// @route POST api/users/saveSearch
+// @description Save a user's searches
+// @access Public
+router.put("/saveSearch", async (req, res) => {
+  res.send(`<p>Trying to save a user's search: ${req.body}!</p>`);
+});
 
 export default router;
