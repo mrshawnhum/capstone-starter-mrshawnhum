9409cff9a9136f228726b0f8d05ad3373f0e4411
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-20 14:24:22 -0500

    Organize Input component for modularity

---
+++ src/components/Forms/Input/index.js
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import camelCase from "camelCase";
+
+export class Input extends React.Component {
+  static defaultProps = {
+    type: "text",
+    value: "value",
+  };
+
+  static propTypes = {
+    label: PropTypes.string.isRequired,
+    onChange: PropTypes.func,
+    st: PropTypes.string,
+    type: PropTypes.string,
+  };
+
+  camelCaseLabel = camelCase(this.props.label);
+
+  render() {
+    return (
+      <div>
+        <label htmlFor={this.camelCaseLabel}>{this.props.label}</label>
+        <input
+          id={this.camelCaseLabel}
+          type={this.props.type}
+          placeholder={this.props.label}
+          data-st={this.props.st || this.camelCase}
+          onChange={this.props.onChange}
+        />
+      </div>
+    );
+  }
+}
--- src/components/Main/Header/index.js
+++ src/components/Main/Header/index.js
@@ -7,7 +7,7 @@ export class Header extends React.Component {
   render() {
     return (
       <div>
-      <h1 className="title">Find My Rescue</h1>
+        <h1 className="title">Find My Rescue</h1>
         <PetSearchBar />
         <Nav />
       </div>
