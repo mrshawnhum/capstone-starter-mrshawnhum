7aaafba0a664d6f4205491ee46067bfddffd350b
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-20 07:29:28 -0500

    Re-arrange components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { Home } from "./Home/";
+export { Main } from "./Main";
 // export { Match } from "./MatchPage";
 // export { PetCard } from "./PetCard/";
 // export { PetSearchBar } from "./PetSearchBar/";
