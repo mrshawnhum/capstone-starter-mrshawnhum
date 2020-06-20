f3f834e3865e1e7a469d180900c11ee28671adf0
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-20 07:30:13 -0500

    Test file

---
+++ src/components/Main/index.js
@@ -0,0 +1,9 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Header } from "./Header";
+
+export class Main extends React.Component {
+  render() {
+    return <Header />;
+  }
+}
