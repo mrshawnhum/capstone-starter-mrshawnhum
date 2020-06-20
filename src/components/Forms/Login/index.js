e0a2b1cfecda2abb062c76314b9a62d22cc3d242
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-20 15:58:57 -0500

    Working login form layout, log input

--- src/components/Forms/Login/index.js
+++ src/components/Forms/Login/index.js
@@ -1,5 +1,37 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-import Form from "../Form";
-export class Login extends Form {}
+import { Button } from "components/Button";
+import { Form } from "../Form";
+export class Login extends Form {
+  state = {
+    isLogged: false,
+    password: "",
+    username: "",
+  };
+
+  inputs = [
+    {
+      inputType: "text",
+      labelText: "Username",
+    },
+    {
+      inputType: "password",
+      labelText: "Password",
+    },
+  ];
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.processFormData(e.target, "st"));
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Button />
+        <Button buttonClass="plain" label="Register?" type="button" />
+      </form>
+    );
+  }
+}
