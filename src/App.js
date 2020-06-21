5f63134ca31029df3b3ad5b3e01e7f5d5ea868c7
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-20 23:21:20 -0500

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import { Home, Login, Main } from "components";
 
 export const App = () => (
-  <Fragment>
-    {<Main />}
-    {<Login />}
-    {<Home />}
-  </Fragment>
+  <Router>
+    <Fragment>
+      {<Main />}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/Login" component={Login} />
+      </Switch>
+    </Fragment>
+  </Router>
 );
--- src/components/Main/Header/Nav/index.js
+++ src/components/Main/Header/Nav/index.js
@@ -1,25 +1,25 @@
 import "bulma/css/bulma.css";
 import PropTypes from "prop-types";
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Nav.css";
 
 export class Nav extends React.Component {
   render() {
     return (
-      <div className="tabs">
+      <nav className="tabs">
         <ul>
-          <li className="is-active">
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Rescue Match</a>
-          </li>
-          <li>
-            <a>Rescue Pets Near Me</a>
-          </li>
+          <Link to="/">
+            <li className="is-active">Home</li>
+          </Link>
+          <li>Rescue Match</li>
+          <li>Rescue Pets Near Me</li>
+          <Link to="/Login">
+            <li>Login</li>
+          </Link>
         </ul>
-      </div>
+      </nav>
     );
   }
 }
