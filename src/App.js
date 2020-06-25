e9ceea9d3ca36519466498d6ab4f33a19d369894
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-25 03:31:01 -0500

    Style Home Page
    Add city, state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { Filters } from "./components/Forms/";
 import { getAllPets } from "api/";
 
 import capitalize from "lodash.capitalize";
+import { Button } from "components/Button";
+
+import "App.css";
+import "Loader.css";
 
 export class App extends React.Component {
   state = {
@@ -34,21 +38,37 @@ export class App extends React.Component {
   renderCards = (animals) =>
     animals.map((animal) => {
       return (
-        <div className="card" key={animal.id}>
-          <div className="card-image">
-            <figure className="image is-4by3">
+        <div className="App card columns is-multiline" key={animal.id}>
+          <div className="column">
+            <figure className="image">
               <img
+                className="CardImg"
                 src={
                   animal.primary_photo_cropped
                     ? animal.primary_photo_cropped.small
                     : "https://via.placeholder.com/150"
                 }
-                alt={`${animal.size} - ${animal.species}`}
+                alt={`${animal.size}
+                ${animal.species}`}
               />
+              <div className="Info">
+                <figcaption className="card-content">{animal.name}</figcaption>
+                <figcaption className="card-content">
+                  {animal.breeds.primary}
+                </figcaption>
+                <figcaption className="card-content">
+                  {animal.contact.address.city}, {animal.contact.address.state}
+                </figcaption>
+                <div>
+                  <a href={`${animal.url}`}>
+                    <button className="Learn button" type="button">
+                      Learn More!
+                    </button>
+                  </a>
+                </div>
+              </div>
             </figure>
           </div>
-
-          <div className="content">{animal.description}</div>
         </div>
       );
     });
@@ -61,12 +81,14 @@ export class App extends React.Component {
         )
       : this.state.pets;
 
-    return (
+    return this.state.pets.length ? (
       <div className="container">
         <HeroLogin />
         <Filters onChange={this.handleChange} />
         {this.renderCards(filteredPets)}
       </div>
+    ) : (
+      <div className="loader">Loading...</div>
     );
   }
 }
